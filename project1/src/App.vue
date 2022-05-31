4df96213c7e57f18b3d737157771fc7fa64840cc
TobeyKwon <tobeykwon@gmail.com>
2022-05-31 21:19:09 +0900

    0531 vue study

--- project1/src/App.vue
+++ project1/src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <modal-view v-if="modalStatus" :product="product" @closeModal="closeModal" />
-  <div class="menu">
-    <a href="" v-for="(메뉴, i) in 메뉴들" :key="i">{{ 메뉴 }}</a>
-  </div>
-  <div v-for="(p, i) in products" :key="i">
-    <product-detail :product="p" @openModal="openModal(p)" />
+  <div>
+    <!-- <div class="start" :class="{ end: modalStatus }">
+      <modal-view
+        v-if="modalStatus"
+        :product="product"
+        @closeModal="closeModal"
+      />
+    </div> -->
+    <transition name="fade">
+      <discount-view v-if="showDiscount" />
+    </transition>
+    <button @click="sortProducts">가격순정렬</button>
+    <button @click="sortBack">되돌리기</button>
+
+    <transition name="fade">
+      <modal-view
+        v-if="modalStatus"
+        :product="product"
+        @closeModal="closeModal"
+      />
+    </transition>
+
+    <div class="menu">
+      <a href="" v-for="(메뉴, i) in 메뉴들" :key="i">{{ 메뉴 }}</a>
+    </div>
+    <product-detail
+      v-for="(p, i) in products"
+      :key="i"
+      :product="p"
+      @openModal="openModal(p)"
+    />
   </div>
 </template>
 
@@ -12,6 +37,7 @@
 import importData from "@/assets/oneroom";
 import ModalView from "@/components/ModalView";
 import ProductDetail from "@/components/ProductDetail.vue";
+import DiscountView from "@/components/DiscountView.vue";
 
 export default {
   name: "App",
@@ -21,6 +47,8 @@ export default {
       메뉴들: ["Home", "Shop", "About"],
       products: importData,
       product: null,
+      originalProducts: [...importData],
+      showDiscount: true,
     };
   },
   methods: {
@@ -32,10 +60,23 @@ export default {
       this.modalStatus = false;
       this.product = null;
     },
+    sortProducts() {
+      this.products.sort((a, b) => a.price - b.price);
+    },
+    sortBack() {
+      this.products = [...this.originalProducts];
+    },
   },
   components: {
     ModalView,
     ProductDetail,
+    DiscountView,
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showDiscount = false;
+      console.log(this);
+    }, 2000);
   },
 };
 </script>
@@ -74,5 +115,34 @@ div {
 .close-btn {
   position: fixed;
   right: 35px;
+}
+
+.start {
+  opacity: 0;
+  transition: all 1s;
+}
+
+.end {
+  opacity: 1;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-leave-from {
+  transform: translateY(0px);
+}
+.fade-leave-active {
+  transition: all 2s;
+}
+.fade-leave-to {
+  transform: translateY(-1000px);
 }
 </style>
